39f7361f5441968830847d52855801dfc79ba73b
yonas790 <yoni2752@gmail.com>
2025-10-29 21:51:13 +0300

    feat: add Assembler to convert assembly code to Instructions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.nio.file.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Java Compiler Project Initialized Successfully!");
+    public static void main(String[] args) throws Exception {
+        List<Instruction> program = Assembler.assemble(Paths.get("programs/test1.asm"));
+        System.out.println("Assembled " + program.size() + " instructions.");
+        for (int i = 0; i < program.size(); i++) System.out.printf("%03d: %s%n", i, program.get(i));
+        StackVM vm = new StackVM(program);
+        vm.run();
     }
 }

f5df028dfadec13642ce3f284a2b405223b1aaf9
yonas790 <yoni2752@gmail.com>
2025-10-29 22:03:25 +0300

    remove test classess

--- src/TestInstr.java
+++
@@ -1,8 +0,0 @@
-public class TestInstr {
-    public static void main(String[] args) {
-        Instruction i1 = new Instruction("PUSH", 5);
-        Instruction i2 = new Instruction("ADD", null);
-        System.out.println(i1);
-        System.out.println(i2);
-    }
-}

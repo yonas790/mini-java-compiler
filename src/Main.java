79f01b80ddf799dd34338de4cf5e6b67da6b953f
yonas790 <yoni2752@gmail.com>
2025-10-31 21:42:56 +0300

    change test file

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        List<Instruction> program = Assembler.assemble(Paths.get("programs/test1.asm"));
+        List<Instruction> program = Assembler.assemble(Paths.get("programs/loop.asm"));
         System.out.println("Assembled " + program.size() + " instructions.");
         for (int i = 0; i < program.size(); i++) System.out.printf("%03d: %s%n", i, program.get(i));
         StackVM vm = new StackVM(program);

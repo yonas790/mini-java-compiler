62fdcac80f5b82933f62f90b11296595fa6c2138
yonas790 <yoni2752@gmail.com>
2025-11-03 14:43:36 +0300

    feat: add token

--- src/compiler/Token.java
+++ src/compiler/Token.java
@@ -1,5 +1,16 @@
 package compiler;
 
 public class Token {
-    
+    public final TokenType type;
+    public final String value;
+
+    public Token(TokenType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return type + (value != null ? "('" + value + "')" : "");
+    }
 }

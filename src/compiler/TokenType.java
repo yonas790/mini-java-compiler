8b6e454bb1abf8c8ec17c0269c7bda7429aca2cc
yonas790 <yoni2752@gmail.com>
2025-11-03 14:42:39 +0300

    feat: add token types

--- src/compiler/TokenType.java
+++ src/compiler/TokenType.java
@@ -1,5 +1,30 @@
 package compiler;
 
-public class TokenType {
+public enum TokenType {
+    // Keywords
+    INT,
+    PRINTF,
     
+    // Symbols
+    SEMICOLON,
+    COMMA,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    ASSIGN,
+    
+    // Operators
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    MOD,
+    
+    // Literals and identifiers
+    NUMBER,
+    IDENTIFIER,
+    
+    // End of file
+    EOF
 }

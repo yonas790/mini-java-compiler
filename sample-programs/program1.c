da4ee9085c227e2d5ae45a022844c59d287332cc
yonas790 <yoni2752@gmail.com>
2025-11-03 16:02:44 +0300

    feat: add Main and testing c code

---
+++ sample-programs/program1.c
@@ -0,0 +1,3 @@
+int x = 10 + 5;
+int y = x * 2;
+printf("%d\n", y);
--- src/compiler/Main.java
+++ src/compiler/Main.java
@@ -1,5 +1,27 @@
 package compiler;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        String input = Files.readString(Path.of("../../sample-programs/program1.c"));
+
+        // 1️⃣ Lexer
+        Lexer lexer = new Lexer(input);
+        List<Token> tokens = lexer.tokenize();
+
+        // 2️⃣ Parser
+        Parser parser = new Parser(tokens);
+        List<Node> ast = parser.parseProgram();
+
+        // 3️⃣ Semantic analysis
+        SemanticAnalyzer analyzer = new SemanticAnalyzer();
+        analyzer.analyze(ast);
+
+        // 4️⃣ Interpreter
+        Interpreter interpreter = new Interpreter();
+        interpreter.execute(ast);
+    }
 }
